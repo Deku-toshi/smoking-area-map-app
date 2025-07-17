c9b8b63121ebda43e024781ab3f2feada04f0733
deku <liberty3toshi@gmail.com>
2025-07-17 22:26:45 +0900

    Add ReportStatus model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_17_132249) do
+ActiveRecord::Schema[7.1].define(version: 2025_07_17_132531) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "report_statuses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "smoking_area_statuses", force: :cascade do |t|
     t.string "name"
