497aed8286ac4f5a38fab06802a47097709c0e13
deku <liberty3toshi@gmail.com>
2025-08-03 19:24:50 +0900

    Fix missing comma in SmokingAreaType seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ SmokingAreaType.create!([
     {name: "公共", icon: "public", color: "#1976D2"},
     {name: "施設内", icon: "mall", color: "#43A047"},
     {name: "飲食店", icon: "restaurant", color: "#8D6E63"},
-    {name: "カフェ", icon: "cafe", color: "#795548"}
+    {name: "カフェ", icon: "cafe", color: "#795548"},
     {name: "コンビニ", icon: "convenience", color: "#FB8C00"},
     {name: "その他", icon: "other", color: "#9E9E9E"}
 ])
