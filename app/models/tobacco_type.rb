df646fd1b5a2bea392106bcd86f083bf885c64b3
deku <liberty3toshi@gmail.com>
2025-08-26 20:40:55 +0900

    refactor(tobacco_type): add length validations

--- app/models/tobacco_type.rb
+++ app/models/tobacco_type.rb
@@ -2,6 +2,6 @@ class TobaccoType < ApplicationRecord
     has_many :smoking_area_tobacco_types
     has_many :smoking_areas, through: :smoking_area_tobacco_types
 
-    validates :kinds, presence: true, uniqueness: true
-    validates :icon, presence: true
+    validates :kinds, presence: true, length: {maximum: 30}, uniqueness: true
+    validates :icon, presence: true, length: {maximum: 255}
 end
