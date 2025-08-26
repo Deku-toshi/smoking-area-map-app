5e5cb8204dcb54fb47269eda2971f4dd7acadba3
deku <liberty3toshi@gmail.com>
2025-08-26 22:45:07 +0900

    refactor(smoking_area_type): add basic validations (name/code/icon/color)

--- app/models/smoking_area_type.rb
+++ app/models/smoking_area_type.rb
@@ -1,2 +1,8 @@
 class SmokingAreaType < ApplicationRecord
+    has_many :smoking_areas
+
+    validates :name, presence: true, length: {maximum: 30}
+    validates :code, presence: true, length: {maximum: 50}, uniqueness: true
+    validates :icon, presence: true, length: {maximum: 255}
+    validates :color, presence: true, format: { with: /\A#[0-9A-Fa-f]{6}\z/ }
 end
