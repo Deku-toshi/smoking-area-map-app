2e27ebdf08bc7b3163a2497a91e5e94f0a2ddcdb
deku <liberty3toshi@gmail.com>
2025-08-27 23:27:14 +0900

    refactor(comment): add has_many :reports, as: :targetable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :smoking_area
 
+  has_many :reports, as: :targetable
+
   validates :content, presence: true, length: {maximum: 1000}
 end
