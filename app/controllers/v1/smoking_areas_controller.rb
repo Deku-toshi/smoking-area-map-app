99aa290dcf65eb703a4e1c174070767f6c2a53aa
deku <liberty3toshi@gmail.com>
2026-04-29 14:08:54 +0900

    refactor: remove show Action from controller

--- app/controllers/v1/smoking_areas_controller.rb
+++ app/controllers/v1/smoking_areas_controller.rb
@@ -31,25 +31,4 @@ class V1::SmokingAreasController < ApplicationController
       }
     end)
   end
-
-  def show
-    smoking_area = SmokingArea.includes(:tobacco_types).find(params[:id])
-
-    render json: { 
-      id:   smoking_area.id,
-      name: smoking_area.name,
-      latitude:  smoking_area.latitude,
-      longitude: smoking_area.longitude,
-      is_indoor: smoking_area.is_indoor,
-      detail:    smoking_area.detail,
-      address:   smoking_area.address,
-      tobacco_types: smoking_area.tobacco_types.map do |tobacco_type|
-         {
-           id:   tobacco_type.id,
-           name: tobacco_type.name,
-           icon: tobacco_type.icon
-         }
-      end
-    }
-  end
 end
